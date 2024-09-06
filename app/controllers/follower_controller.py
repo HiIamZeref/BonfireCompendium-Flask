6590fbda2076e1256173c6b566e0949e87adb658
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-09-06 16:18:41 -0300

    :hammer: refactor follow route~

--- app/controllers/follower_controller.py
+++ app/controllers/follower_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from app.services.follower_service import FollowerService
-from app.schemas.follower_schema import FollowerSchema
+from app.schemas.follower_schema import FollowerSchema, CreateOrDeleteFollowerSchema
+from marshmallow import ValidationError
 
 # Blueprint for followers
 followers = Blueprint('followers', __name__)
@@ -9,14 +10,34 @@ followers = Blueprint('followers', __name__)
 def follow(follower_service: FollowerService = FollowerService()):
     """Follow a user."""
     data = request.get_json()
-    follower = follower_service.follow(data)
+    create_follower_schema = CreateOrDeleteFollowerSchema()
+
+    try:
+        validated_data = create_follower_schema.load(data)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    follower = follower_service.follow(validated_data)
+    
     return jsonify(FollowerSchema().dump(follower)), 201
 
 def unfollow(follower_service: FollowerService = FollowerService()):
     """Unfollow a user."""
     data = request.get_json()
-    follower_service.unfollow(data)
-    return jsonify({'message': 'Unfollowed successfully'}), 200
+    follower_schema = CreateOrDeleteFollowerSchema()
+
+    try:
+        validated_data = follower_schema.load(data)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+
+    try:
+        follower_service.unfollow(validated_data)
+    except ValueError as err:
+        return jsonify({'message': 'Already unfollowed.'}), 404
+    
+    return jsonify({'message': 'Unfollowed successfully'}), 204
 
 def get_followers(user_id, follower_service: FollowerService = FollowerService()):
     """Get all followers for a given user."""
