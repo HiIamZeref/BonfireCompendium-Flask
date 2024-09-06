1d640fdf5da07f34a5cb8167a07b52917c5c82cf
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-09-06 17:50:40 -0300

    created CreateOrDeleteSchema

--- app/schemas/follower_schema.py
+++ app/schemas/follower_schema.py
@@ -4,6 +4,19 @@ class FollowerSchema(Schema):
     '''
     Schema for Follower model.
     '''
-    id = fields.Integer(dump_only=True)
+    user = fields.Method("get_user_username")
+    follower_username = fields.Method("get_follower_username")
+
+    def get_user_username(self, obj):
+        return obj.user.username
+    
+    def get_follower_username(self, obj):
+        return obj.follower.username
+    
+
+class CreateOrDeleteFollowerSchema(Schema):
+    '''
+    Schema for creating a Follower object.
+    '''
     user_id = fields.Integer(required=True)
     follower_id = fields.Integer(required=True)
