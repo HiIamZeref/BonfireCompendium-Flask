4164470cec2fa138ab26bf0aa30951412bc721e6
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-09-06 15:41:09 -0300

    :hammer: add: relations in Follower model

--- app/models/follower.py
+++ app/models/follower.py
@@ -6,6 +6,10 @@ class Follower(db.Model):
     # Columns
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     follower_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+
+    # Relationships
+    user = db.relationship('User', foreign_keys=[user_id], backref='user_followers')
+    follower = db.relationship('User', foreign_keys=[follower_id], backref='user_following')
     
     def __repr__(self):
         return f'<Follower {self.user_id} follows {self.follower_id}>'
