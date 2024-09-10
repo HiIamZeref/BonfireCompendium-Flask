54997a22e1723e2470d85708c290a53d8929c862
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-09-10 17:06:35 -0300

    deleted single user

--- app/seeds/user_seeds.py
+++ app/seeds/user_seeds.py
@@ -2,13 +2,6 @@ from app.repositories.user_repository import UserRepository
 from app.schemas.user_schema import UserSchema
 
 def seed_users(user_repository: UserRepository= UserRepository()):
-    user = {
-        'email': 'felipe@email.com',
-        'username': 'gaby',
-        'password': '1234',
-        'bio': 'I am a software engineer',
-    }
-
     users = [
         {
         'email': 'felipe@email.com',
