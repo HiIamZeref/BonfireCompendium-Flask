1cb841d67fdbd0544c2768574849ed7a260ac016
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-09-10 17:21:15 -0300

    add: user routes to router

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from app.controllers.developer_controller import developers
 from app.controllers.follower_controller import followers
+from app.controllers.game_controller import games
 from app.controllers.game_status_controller import game_statuses
 from app.controllers.genre_controller import genres
 from app.controllers.platform_controller import platforms
@@ -15,6 +16,7 @@ api = Blueprint('api', __name__)
 # Registering blueprints
 api.register_blueprint(developers, url_prefix='/developers')
 api.register_blueprint(followers, url_prefix='/followers')
+api.register_blueprint(games, url_prefix='/games')
 api.register_blueprint(game_statuses, url_prefix='/game_statuses')
 api.register_blueprint(genres, url_prefix='/genres')
 api.register_blueprint(platforms, url_prefix='/platforms')
