e39e1f57a45a3b841933e4d65279edf4116546b0
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-09-10 17:07:21 -0300

    created CreateOrDelete Schema and refactored GameSchema

--- app/schemas/game_schema.py
+++ app/schemas/game_schema.py
@@ -5,7 +5,30 @@ class GameSchema(Schema):
     Schema for Game model.
     '''
     id = fields.Integer(dump_only=True)
-    name = fields.Str(required=True)
+    title = fields.Str(required=True)
+    description = fields.Str(required=True)
+    release_date = fields.Date(required=True)
+    genre = fields.Method("get_genre")
+    developer = fields.Method("get_developer")
+    publisher = fields.Method("get_publisher")
+    cover_image = fields.Str()
+
+    def get_genre(self, obj):
+        return obj.genre.name
+    
+    def get_developer(self, obj):
+        return obj.developer.name
+    
+    def get_publisher(self, obj):
+        return obj.publisher.name
+
+
+class CreateOrDeleteGameSchema(Schema):
+    '''
+    Schema for creating or deleting a Game object.
+    '''
+    id = fields.Integer(dump_only=True)
+    title = fields.Str(required=True)
     description = fields.Str(required=True)
     release_date = fields.Date(required=True)
     genre_id = fields.Integer(required=True)
