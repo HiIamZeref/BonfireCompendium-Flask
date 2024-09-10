1f55ad10120cd8e90649beefd330c95ddd975283
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-09-10 16:16:43 -0300

    add: more users

--- app/seeds/user_seeds.py
+++ app/seeds/user_seeds.py
@@ -9,9 +9,30 @@ def seed_users(user_repository: UserRepository= UserRepository()):
         'bio': 'I am a software engineer',
     }
 
-    user = UserSchema().load(user)
+    users = [
+        {
+        'email': 'felipe@email.com',
+        'username': 'gaby',
+        'password': '1234',
+        'bio': 'I am a software engineer',
+        },
+        {
+        'email': 'bibia@email.com',
+        'username': 'bibiagaby',
+        'password': '4321',
+        'bio': 'I am a financial analyst',
+        },
+        {
+        'email': 'haru@email.com',
+        'username': 'kingharu',
+        'password': '5678',
+        'bio': 'I am the king of all that is golden!',
+        }
+    ]
+    for user in users:
+        user = UserSchema().load(user)
+        user_repository.create(user)
 
-    user_repository.create(user)
     print('Users seeded!')
 
 
