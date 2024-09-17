af3c3e607353a059cb8b3172dfbde6104a1cc0de
HiIamZeref <felipegabycoimbra@hotmail.com>
2024-09-17 15:04:34 -0300

    :wrench: fixed game seeds

--- app/seeds/game_seeds.py
+++ app/seeds/game_seeds.py
@@ -1,10 +1,10 @@
 from app.repositories.game_repository import GameRepository
-from app.schemas.game_schema import GameSchema
+from app.schemas.game_schema import CreateOrDeleteGameSchema
 
 def seed_games(game_repository: GameRepository = GameRepository()):
     games = [
         {
-            'name': 'The Legend of Zelda: Breath of the Wild',
+            'title': 'The Legend of Zelda: Breath of the Wild',
             'description': 'The Legend of Zelda: Breath of the Wild is an action-adventure game developed and published by Nintendo. An entry in the longrunning The Legend of Zelda series, it was released for the Nintendo Switch and Wii U consoles on March 3, 2017.',
             'release_date': '2017-03-03',
             'genre_id': 4,
@@ -12,7 +12,7 @@ def seed_games(game_repository: GameRepository = GameRepository()):
             'publisher_id': 5,
         },
         {
-            'name': 'Dark Souls III',
+            'title': 'Dark Souls III',
             'description': 'Dark Souls III is an action role-playing game developed by FromSoftware and published by Bandai Namco Entertainment. It was released for PlayStation 4, Xbox One, and Microsoft Windows in Japan in March 2016 and worldwide in April 2016.',
             'release_date': '2016-03-24',
             'genre_id': 2,
@@ -20,7 +20,7 @@ def seed_games(game_repository: GameRepository = GameRepository()):
             'publisher_id': 1,
         },
         {
-            'name': 'Bloodborne',
+            'title': 'Bloodborne',
             'description': 'Bloodborne is an action role-playing game developed by FromSoftware and published by Sony Computer Entertainment, which released for the PlayStation 4 in March 2015.',
             'release_date': '2015-03-24',
             'genre_id': 2,
@@ -28,7 +28,7 @@ def seed_games(game_repository: GameRepository = GameRepository()):
             'publisher_id': 2,
         },
         {
-            'name': 'The Witcher 3: Wild Hunt',
+            'title': 'The Witcher 3: Wild Hunt',
             'description': 'The Witcher 3: Wild Hunt is an action role-playing game developed and published by CD Projekt. Based on The Witcher series of fantasy novels by Polish author Andrzej Sapkowski, it is the sequel to the 2011 game The Witcher 2: Assassins of Kings.',
             'release_date': '2015-05-19',
             'genre_id': 5,
@@ -36,7 +36,7 @@ def seed_games(game_repository: GameRepository = GameRepository()):
             'publisher_id': 4,
         },
         {
-            'name': 'Red Dead Redemption 2',
+            'title': 'Red Dead Redemption 2',
             'description': 'Red Dead Redemption 2 is a 2018 action-adventure game developed and published by Rockstar Games. The game is the third entry in the Red Dead series and is a prequel to the 2010 game Red Dead Redemption.',
             'release_date': '2018-10-26',
             'genre_id': 4,
@@ -44,7 +44,7 @@ def seed_games(game_repository: GameRepository = GameRepository()):
             'publisher_id': 3,
         },
         {
-            'name': 'Grand Theft Auto V',
+            'title': 'Grand Theft Auto V',
             'description': 'Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games. It is the first main entry in the Grand Theft Auto series since 2008\'s Grand Theft Auto IV.',
             'release_date': '2013-09-17',
             'genre_id': 4,
@@ -52,7 +52,7 @@ def seed_games(game_repository: GameRepository = GameRepository()):
             'publisher_id': 3,
         },
         {
-            'name': 'Persona 5 Royal',
+            'title': 'Persona 5 Royal',
             'description': 'Persona 5 Royal is a role-playing video game developed by Atlus. It is a re-release of Persona 5, which was released for the PlayStation 3 and PlayStation 4 in 2016.',
             'release_date': '2020-03-31',
             'genre_id': 5,
@@ -60,7 +60,7 @@ def seed_games(game_repository: GameRepository = GameRepository()):
             'publisher_id': 6,
         },
         {
-            'name': 'Elden Ring',
+            'title': 'Elden Ring',
             'description': 'Elden Ring is an upcoming action role-playing game developed by FromSoftware and published by Bandai Namco Entertainment. It is set to be released for Microsoft Windows, PlayStation 4, and Xbox One.',
             'release_date': '2022-02-25',
             'genre_id': 2,
@@ -68,3 +68,9 @@ def seed_games(game_repository: GameRepository = GameRepository()):
             'publisher_id': 1,
         }
     ]
+
+    for game in games:
+        game = CreateOrDeleteGameSchema().load(game)
+        game_repository.create(game)
+
+    print('Games seeded!')
